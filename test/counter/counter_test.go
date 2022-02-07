31774d1ce9f83807d845442f5bb30fce2751f5a2
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-02-07 11:16:26 +0100

    counting input test

---
+++ test/counter/counter_test.go
@@ -0,0 +1,31 @@
+package zoo_test
+
+import (
+	"testing"
+
+	"github.com/ditrit/specimen/go/specimen"
+)
+
+var counter = 0
+
+func TestFocusZoo(t *testing.T) {
+	specimen.Run(
+		t,
+		specimen.MakeCodeboxSet(map[string]specimen.BoxFunction{
+			"counter": func(s *specimen.S, input specimen.Dict) {
+				expected, found := input["expected_count"]
+				if found {
+					s.ExpectEqual(
+						counter,
+						expected.(int),
+						"count comparison",
+					)
+				}
+				counter += 1
+			},
+		}),
+		[]specimen.File{
+			specimen.ReadLocalFile("counter.spec.yaml"),
+		},
+	)
+}
